36d3cbfb2227f3058a5acc1a20c7a8228ed845be
swaroop <swaroopanand325@gmail.com>
2020-02-23 01:49:35 +0530

    mongodb connection to atlas

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 'use strict'
 
-var express  = require('express');
-var app      = express();
-var port     = process.env.PORT || 8000;
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8000;
 
-var mongoose    = require('mongoose');
-var database    = require('./database');
+var mongoose = require('mongoose');
+var database = require('./database');
 
 // Pull information from HTML POST (express4)
-var bodyParser     = require('body-parser');
+var bodyParser = require('body-parser');
 
+const dbURI = "mongodb+srv://user:user@cluster0-tuhhf.mongodb.net/test?retryWrites=true&w=majority"
 // Mongoose connection
-mongoose.connect('mongodb+srv://user:user@cluster0-tuhhf.mongodb.net/test?retryWrites=true&w=majority');
+const options = {
+    reconnectTries: Number.MAX_VALUE,
+    poolSize: 10
+};
 
+mongoose.connect(dbURI, options).then(
+    () => {
+        console.log("Database connection established!");
+    },
+    err => {
+        console.log("Error connecting Database instance due to: ", err);
+    }
+);
 // Parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.urlencoded({ 'extended': 'true' }));
 
 // Parse application/json
 app.use(bodyParser.json());
