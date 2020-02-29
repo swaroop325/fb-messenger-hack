0e2ee7bee74ddf04b4dc51753cd05fa11b465ffc
swaroop <swaroopanand325@gmail.com>
2020-03-01 04:26:02 +0530

    n

--- user.service.js
+++ user.service.js
@@ -1,13 +1,12 @@
 const User = require('./user');
 const request = require('request');
-const mongoose = require('mongoose');
 const Mongoose = require("mongoose"),
   Types = Mongoose.Schema.Types;
 //Employee Model without any fixed schema
 const logCollectionSchema = new Mongoose.Schema({},
   { strict: false }
 );
-const LogCollection = mongoose.model('log_collection', logCollectionSchema)
+const LogCollection = Mongoose.model('log_collection', logCollectionSchema)
 
 module.exports = {
   getFacebookData: getFacebookData,
