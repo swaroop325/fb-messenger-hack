6f1c7000c4f93748a301ea49df75764d00b3a1ed
swaroop <swaroopanand325@gmail.com>
2020-03-02 23:34:32 +0530

    added intent recogniotion

--- api-ai.js
+++ api-ai.js
@@ -1,16 +1,20 @@
 const userService = require('./user.service');
-
+const apiCalls = require('./database')
 module.exports = function(app) {
   // Index route
   app.get('/', function (req, res) {
     res.send('Welcome to the Index Route');
   });
+  
   // API.AI webhook route
   app.post('/webhook/apiai/', function(req, res) {
-    res.status(200).json('Sucessfull');
-    console.log(req.queryResult)
+    if(req.body.queryResult.intent.displayName == "product"){
+//call other service
+    }else{
+      res.status(200).json('Sucessfull');
+    }
     // Save User to MongoDB
-   // userService.saveUser(req, res ,req.body.originalDetectIntentRequest.payload.data.sender.id);
+   userService.saveUser(req, res ,req.body.originalDetectIntentRequest.payload.data.sender.id);
   });
 }
 
