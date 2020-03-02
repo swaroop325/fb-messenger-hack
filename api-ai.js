6f3f2b0c745936233a74aa1396c32413d8594a7b
swaroop <swaroopanand325@gmail.com>
2020-03-02 23:04:26 +0530

    d

--- api-ai.js
+++ api-ai.js
@@ -8,9 +8,9 @@ module.exports = function(app) {
   // API.AI webhook route
   app.post('/webhook/apiai/', function(req, res) {
     res.status(200).json('Sucessfull');
-
+    console.log(req.queryResult)
     // Save User to MongoDB
-    userService.saveUser(req, res ,req.body.originalDetectIntentRequest.payload.data.sender.id);
+   // userService.saveUser(req, res ,req.body.originalDetectIntentRequest.payload.data.sender.id);
   });
 }
 
